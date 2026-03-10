7b43b5c386ca83720199c806906064b670938dec
Nick Guo <shuangnick2004@gmail.com>
2026-03-10 18:51:05 +1100

    feat: some code updates from last week?

--- coded-solutions/week3/Untitled.R
+++ coded-solutions/week3/Untitled.R
@@ -1,40 +1,20 @@
+# install.packages('ISLR2')
 library(ISLR2)
 
+
 df = ISLR2::Carseats
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Filler line
 
 
-fit <- lm(Sales ~ Price + Urban + US, data = Carseats)
+fit = lm(Sales ~ Price + Urban + US, data = Carseats)
+
 summary(fit)
+
+# filler line
+
+
+
 
 fit2 <- lm(Sales ~ Price + US, data = Carseats)
 summary(fit2)
