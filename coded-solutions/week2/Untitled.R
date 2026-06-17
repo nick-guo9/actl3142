b7ad56cd302b95b4f989655a78ce580a50d34975
Nick Guo <shuangnick2004@gmail.com>
2026-06-17 17:25:24 +1000

    code

--- coded-solutions/week2/Untitled.R
+++ coded-solutions/week2/Untitled.R
@@ -1,36 +1,26 @@
-# df = read.csv("~/Downloads/airbnb-London.csv")
-# 
-# str(df)
-# 
-# hist(df$price_per_room)
-
-
-
-
-# install.packages("ISLR2")
-
 library(ISLR2)
-
-
 
 df = Auto
 
 
-
 fit = lm(mpg ~ horsepower, data = df)
+
+
 summary(fit)
-# fit <- lm(y ~ x1 + x2 + .., data = Auto)
 
 
-df['feat_1'] = df['horsepower'] ^ (1/2)
+
+
+
+
+
+
 
 
 
 fit <- lm(mpg ~ horsepower + weight + acceleration + year + displacement, data = Auto)
-
 summary(fit)
-
-
+plot(fit)
 
 
 
@@ -46,7 +36,7 @@ predict(fit, newdata = data.frame(horsepower = c(98)), interval = "prediction")
 
 
 
-plot(df$feat_1, df$mpg)
+plot(df$horsepower, df$mpg)
 abline(a = fit$coefficients[1], b = fit$coefficients[2])
 
 
@@ -62,3 +52,13 @@ plot(fit)
 
 
 
+df['feat_1'] = df['horsepower'] ^ (1 / 2)
+fit <- lm(mpg ~ feat_1, data = df)
+plot(df$feat_1, df$mpg)
+abline(a = fit$coefficients[1], b = fit$coefficients[2])
+
+
+df['feat_1'] = df['horsepower'] ^ (1)
+fit <- lm(mpg ~ feat_1, data = df)
+plot(df$feat_1, df$mpg)
+abline(a = fit$coefficients[1], b = fit$coefficients[2])
