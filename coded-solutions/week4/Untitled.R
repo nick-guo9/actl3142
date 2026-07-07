2c7c55563fa8c12a8de5f1194175874c500dcfb3
Nick Guo <shuangnick2004@gmail.com>
2026-07-07 14:05:53 +1000

    week 7 slides updated and old code upload

--- coded-solutions/week4/Untitled.R
+++ coded-solutions/week4/Untitled.R
@@ -21,6 +21,12 @@ lines(density(df$Lag1), col = "green", lwd = 3.5) # Adds a smooth trend line
 
 
 
+model = glm(
+  Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume,
+  family = binomial(link = "logit"),
+  data = df
+  )
+summary(model)
 
 
 
@@ -39,10 +45,12 @@ lines(density(df$Lag1), col = "green", lwd = 3.5) # Adds a smooth trend line
 
 
 
-
-
-
-
+model = glm(
+  Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume,
+  family = binomial(link = "logit"),
+  data = df
+  )
+summary(model)
 
 
 
@@ -55,15 +63,20 @@ summary(fit)
 fit$y[1] # check how R is encoding "Up" and "Down"
 
 
-pred <- rep("Up", length(df$Direction))
-pred[fit$fitted.values < 0.5] <- "Down"
+
+
+
+pred <- rep("Down", length(df$Direction))
+
+
+pred[model$fitted.values > 0.5] <- "Up"
 table(pred, actual=df$Direction)
 dim(df)
 (48 + 430) / 1089
 
 sum(df$Direction == "Up")
 sum(df$Direction == "Down")
-
+605/1089
 
 
 
@@ -91,10 +104,6 @@ pred.fit2 <- predict(fit2, newdata = df[!train, ], type = "response")
 pred.val <- rep("Down", length(pred.fit2))
 pred.val[pred.fit2 > 0.5] <- "Up"
 table(pred.val, actual=df$Direction[!train])
-
-
-
-(5 + 34) / (9 + 5 + 34 + 56)
 
 
 
@@ -133,3 +142,47 @@ table(fit5, actual=df$Direction[!train])
 
 (22 + 29) / (21 + 29 + 22 + 32)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(MASS)
+library(corrplot)
+library(ggplot2)
+df = ISLR2::Weekly
+head(df)
+pairs(df)
+ggplot(
+  data = df,
+  mapping = aes(x = Lag1, y = Direction),
+  ) + geom_boxplot()
+
+corrplot(cor(df[, -9]), method = "color", addCoef.col = "black")
+
+hist(df$Lag1, prob = TRUE, col = "lightgrey")
+lines(density(df$Lag1), col = "green", lwd = 3.5)
+
+model = glm(Direction ~ Volume + Lag1 + Lag2 + Lag3 + Lag4 + Lag5, data = df, family = binomial(link = "logit"))
+summary(model)
+tmp = model$y
+tmp[1000]
+
+pred = rep("Down",length(df$Direction))
+pred[model$fitted.values > 0.5] = "Up"
+table(pred, actual=df$Direction)
+
+
+train = df[df['Year'] <= 2008, ]
+head(train)
